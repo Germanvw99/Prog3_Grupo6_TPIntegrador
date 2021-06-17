9eba3c59f7cbc318f47010d9b9c4c05d89274824
arielst2021 <arieltapia42@gmail.com>
2021-06-17 10:07:21 -0300

    entidad mediosPago

---
+++ Entidades/MediosPago.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Entidades
+{
+    public class MediosPago
+    {
+        private int mp_codigo;
+        private string mp_nombre;
+        private string mp_otros_detalles;
+        public MediosPago() { }
+        public int GetCodigo() { return mp_codigo; }
+        public string GetNombre() { return mp_nombre; }
+        public string GetImagenUrl() { return mp_otros_detalles; }
+        public void SetCodigo(int mp_codigo) { this.mp_codigo = mp_codigo; }
+        public void SetNombre(string mp_nombre) { this.mp_nombre = mp_nombre; }
+        public void SetImagenUrl(string mp_otros_detalles) { this.mp_otros_detalles = mp_otros_detalles; }
+    }
+}
