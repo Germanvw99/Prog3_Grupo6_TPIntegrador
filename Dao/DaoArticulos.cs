99afdc051a1f93a96bb31158450f9c49467850f6
arielst2021 <arieltapia42@gmail.com>
2021-06-17 10:22:15 -0300

    Update DaoArticulos.cs

--- Dao/DaoArticulos.cs
+++ Dao/DaoArticulos.cs
@@ -112,7 +112,7 @@ namespace Dao
 		public DataTable ObtenerArticulospormarca(string marca)
 		{
 			int marcaa = Convert.ToInt32(marca);
-			string strTabla = "Categorias";
+			string strTabla = "ArticulosMarca";
 			string srtSQL = "SELECT art_codigo,art_nombre,art_descripcion,art_punto_pedido,art_precio_lista,art_ruta_imagen,est_nombre, mar_nombre, cat_nombre FROM Articulos INNER JOIN Estados ON Articulos.art_codigo_estado=Estados.est_codigo INNER JOIN Marcas ON Marcas.mar_codigo=Articulos.art_marca_codigo INNER JOIN Categorias ON Categorias.cat_codigo=Articulos.art_categoria_codigo  where art_marca_codigo = " + marcaa;
 
 			return accDatos.ObtenerTabla(strTabla, srtSQL);
