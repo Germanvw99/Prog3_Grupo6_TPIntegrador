677f49a9e63202d6475634ebb3094a95b4094fe9
arielst2021 <arieltapia42@gmail.com>
2021-06-17 10:15:38 -0300

    detalleVenta Actualizacion

---
+++ Vistas/DetalleVenta.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using Negocio;
+using Entidades;
+
+
+namespace Vistas
+{
+    public partial class DetalleVenta : System.Web.UI.Page
+    {
+        private readonly NegocioDetalleVentas negocioDetalleVentas = new NegocioDetalleVentas();
+        private readonly NegocioVentas negocioVentas = new NegocioVentas();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //if (NegocioUsuarios.getInstance().isAdmin() != true)
+            //{
+            //    Response.Redirect("home.aspx");
+            //}
+
+            string venta_codigo = negocioDetalleVentas.ObtenerSesionDetalleVenta();
+
+            DataTable dt = negocioVentas.ObtenerTablaSesionVentas();
+            foreach (DataRow dr in dt.Rows)
+            {
+                if (venta_codigo.Equals(dr["ven_codigo"].ToString()))
+                {
+                    LblNumeroFactura.Text = dr["ven_codigo"].ToString();
+                    LblFecha.Text = dr["ven_fecha"].ToString();
+                    LblApellidoNombre.Text = dr["usu_apellido"].ToString() + ", " + dr["usu_nombre"].ToString();
+                    LblDni.Text = dr["ven_usuarios_dni"].ToString();
+                    LblDireccion.Text = dr["usu_direccion"].ToString();
+                    LblLocalidad.Text = dr["usu_ciudad"].ToString();
+                    LblTotalFacturado.Text = dr["ven_total_facturado"].ToString();
+                    break;
+                }
+            }
+
+            if (!Page.IsPostBack)
+            {
+
+                CargarGridView(LblNumeroFactura.Text);
+            }
+        }
+        private void CargarGridView(string CodigoVenta)
+        {
+            GrdDetalleVentas.DataSource = negocioDetalleVentas.ObtenerDetalleVentas(CodigoVenta);
+            GrdDetalleVentas.DataBind();
+        }
+    }
+}
---
+++ Vistas/DetalleVenta.aspx.designer.cs
@@ -0,0 +1,89 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Vistas
+{
+
+
+    public partial class DetalleVenta
+    {
+
+        /// <summary>
+        /// Control LblNumeroFactura.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblNumeroFactura;
+
+        /// <summary>
+        /// Control LblFecha.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblFecha;
+
+        /// <summary>
+        /// Control LblApellidoNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblApellidoNombre;
+
+        /// <summary>
+        /// Control LblDni.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblDni;
+
+        /// <summary>
+        /// Control LblDireccion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblDireccion;
+
+        /// <summary>
+        /// Control LblLocalidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblLocalidad;
+
+        /// <summary>
+        /// Control GrdDetalleVentas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GrdDetalleVentas;
+
+        /// <summary>
+        /// Control LblTotalFacturado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblTotalFacturado;
+    }
+}
